421cf3bbd41a6f751ac1f30c3b5e04f727f2bf01
Motoki Saito <stmtk13044032@gmail.com>
2023-11-14 17:51:01 +0900

    右から文字が消えてくアニメーション追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,50 @@
 import { useEffect, useState } from "react";
 import clsx from "clsx";
 
-export default function Home() {
+function FirstExample() {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     setTimeout(() => setIsVisible(!isVisible), 3000);
   }, [setIsVisible, isVisible])
+  const divClassName = "block transition-transform duration-300";
   return (
-    <main className="flex overflow-hidden">
-      <div className={clsx("block transition-transform duration-300", {"translate-y-full" :isVisible})}>T</div>
-      <div className={clsx("block transition-transform duration-300 delay-6", {"translate-y-full" :isVisible})}>E</div>
-      <div className={clsx("block transition-transform duration-300 delay-12", {"translate-y-full" :isVisible})}>X</div>
-      <div className={clsx("block transition-transform duration-300 delay-18", {"translate-y-full" :isVisible})}>T</div>
+    <h1 className="flex overflow-hidden">
+      <div className={clsx(divClassName, {"translate-y-full" :isVisible})}>T</div>
+      <div className={clsx(divClassName, "delay-6", {"translate-y-full" :isVisible})}>E</div>
+      <div className={clsx(divClassName, "delay-12", {"translate-y-full" :isVisible})}>X</div>
+      <div className={clsx(divClassName, "delay-18", {"translate-y-full" :isVisible})}>T</div>
       <div>&nbsp;</div>
-      <div className={clsx("block transition-transform duration-300 delay-30", {"translate-y-full" :isVisible})}>A</div>
-      <div className={clsx("block transition-transform duration-300 delay-36", {"translate-y-full" :isVisible})}>N</div>
-      <div className={clsx("block transition-transform duration-300 delay-42", {"translate-y-full" :isVisible})}>I</div>
-      <div className={clsx("block transition-transform duration-300 delay-48", {"translate-y-full" :isVisible})}>M</div>
-      <div className={clsx("block transition-transform duration-300 delay-54", {"translate-y-full" :isVisible})}>A</div>
-      <div className={clsx("block transition-transform duration-300 delay-60", {"translate-y-full" :isVisible})}>T</div>
-      <div className={clsx("block transition-transform duration-300 delay-66", {"translate-y-full" :isVisible})}>I</div>
-      <div className={clsx("block transition-transform duration-300 delay-72", {"translate-y-full" :isVisible})}>O</div>
-      <div className={clsx("block transition-transform duration-300 delay-78", {"translate-y-full" :isVisible})}>N</div>
+      <div className={clsx(divClassName, "delay-30", {"translate-y-full" :isVisible})}>A</div>
+      <div className={clsx(divClassName, "delay-36", {"translate-y-full" :isVisible})}>N</div>
+      <div className={clsx(divClassName, "delay-42", {"translate-y-full" :isVisible})}>I</div>
+      <div className={clsx(divClassName, "delay-48", {"translate-y-full" :isVisible})}>M</div>
+      <div className={clsx(divClassName, "delay-54", {"translate-y-full" :isVisible})}>A</div>
+      <div className={clsx(divClassName, "delay-60", {"translate-y-full" :isVisible})}>T</div>
+      <div className={clsx(divClassName, "delay-66", {"translate-y-full" :isVisible})}>I</div>
+      <div className={clsx(divClassName, "delay-72", {"translate-y-full" :isVisible})}>O</div>
+      <div className={clsx(divClassName, "delay-78", {"translate-y-full" :isVisible})}>N</div>
+    </h1>
+  )
+}
+
+function SecondExample() {
+  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setIsVisible(!isVisible), 2100);
+  }, [setIsVisible, isVisible])
+  return (
+    <h1 className={clsx("overflow-hidden transition-transform duration-1000", {"-translate-x-full": !isVisible})}>
+      <div className={clsx("transition-transform duration-1000", {"translate-x-full": !isVisible})}>TEXT ANIMATION</div>
+    </h1>
+  )
+}
+
+export default function Home() {
+  return (
+    <main>
+      <FirstExample />
+      <SecondExample />
     </main>
   )
 }
